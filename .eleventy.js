68b9bfc5d93477409db468f2c2782656f4fe0e10
Mark Oliver <mark.oliver@oakinnovate.com>
2026-02-12 16:17:24 +0000

    Fix missing files

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,12 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/bluesky.css");
   eleventyConfig.addPassthroughCopy("src/bluesky.js");
   eleventyConfig.addPassthroughCopy("src/images");
+  eleventyConfig.addPassthroughCopy("src/*.jpg");
+  eleventyConfig.addPassthroughCopy("src/*.png");
+  eleventyConfig.addPassthroughCopy("src/*.svg");
+  eleventyConfig.addPassthroughCopy("src/*.ico");
+  eleventyConfig.addPassthroughCopy("src/staticwebapp.config.json");
+  eleventyConfig.addPassthroughCopy("src/site.webmanifest");
   return {
     dir: {
       input: "src",
