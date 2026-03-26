ab58cd361ddddb90e6809c12850f35dfb6c518a8
Mark Oliver <mark.oliver@oakinnovate.com>
2026-03-26 20:46:55 +0000

    Moving to sessionize for the forms.

--- .eleventy.js
+++ .eleventy.js
@@ -9,14 +9,13 @@ function serializeDom(dom) {
 }
 
 export default function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("src/styles.css");
-  eleventyConfig.addPassthroughCopy("src/bluesky.css");
   eleventyConfig.addPassthroughCopy("src/bluesky.js");
   eleventyConfig.addPassthroughCopy("src/images");
   eleventyConfig.addPassthroughCopy("src/*.jpg");
   eleventyConfig.addPassthroughCopy("src/*.png");
   eleventyConfig.addPassthroughCopy("src/*.svg");
   eleventyConfig.addPassthroughCopy("src/*.ico");
+  eleventyConfig.addPassthroughCopy("src/*.css");
   eleventyConfig.addPassthroughCopy("src/staticwebapp.config.json");
   eleventyConfig.addPassthroughCopy("src/site.webmanifest");
   eleventyConfig.addPassthroughCopy("src/robots.txt");
